734c5509befda03187b624af09a7bf4d6f879905
tomhiggins <tomhiggins@gmail.com>
2016-12-04 23:40:14 -0700

    Add the code and docs

---
+++ js/audioAssets.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var audioData = [
+    {
+        'title' : 'Old Time Radio Broadcasts on this day in history.',
+        'url' : 'https://tomwsmf.com/podcasts/wsmfaday.m3u'
+    },
+    {
+        'title' : 'The Bushel of Apple first broadcast on December 1965',
+        'url' : 'https://www.archive.org/download/JeanShepherd1965Pt1/1965_12_30_Bushel_of_Apples.mp3'
+    }
+];
+
+module.exports = audioData;
